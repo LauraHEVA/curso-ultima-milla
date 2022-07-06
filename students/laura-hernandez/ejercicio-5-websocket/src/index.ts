719547a5cc4d2857b29c28105f213027fa1e3ae0
Laura Hernandez Varela <lhvarela@gmail.com>
2022-07-06 16:52:04 +0200

    Chat exercise with socket io

---
+++ students/laura-hernandez/ejercicio-5-websocket/src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import http from 'http';
+import path from 'path';
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('./src/index.html') );
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(65535, () => {
+  console.log('listening on *:65535');
+});
